bc49b6d2ffadcd03932754a6f58cb8534b1ee86c
rguinalz <guinalz117@gmail.com>
2026-05-06 21:33:31 -0300

    Change workflows

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket  = "terraform-state-rguinalz"
-    key     = "site/terraform.tfstate"
+    key     = "site2/terraform.tfstate"
     region  = "us-east-2"
     encrypt = true
     user_lockfile = true
